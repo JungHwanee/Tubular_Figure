4ae20b770e99dbf9f1bdbc56891691b0d1b36cd4
JungHwan Lee <110312819+JungHwanee@users.noreply.github.com>
2023-04-17 17:51:15 +0900

    Update Cell_Figure_Code.m

--- Cell_Figure_Code.m
+++ Cell_Figure_Code.m
@@ -3,11 +3,12 @@
   
 % assumes .mat has a CCCV Crate (C), Inner Diameter (mm), Charging Time (min), Li-Plating Potential (V) etc...
 
+clear;clc;close all
 
 %% Interface
 
 % .mat (struct) fullpath
-origin_fullpath ='D:\Leejh\1. KENTECH\1. Modeling\COMSOL+MATLAB\Matlab\LJH\Final\Final\Cell_Result.mat';
+origin_fullpath ='D:\Leejh\1. KENTECH\1. Modeling\COMSOL+MATLAB\Matlab\LJH\Final\Final\Cell_Result_v2.mat';
 
 % parameters
 A = 1; % scaling factor
@@ -26,9 +27,25 @@ Crate_unique = unique(Crate_column);
 Diameter_column = cell2mat(data_accum.Diameter);
 Diameter_unique = unique(Diameter_column);
 
-% Charging Time vector
-Time_column = cell2mat(data_accum.Time);
+Charging_Time = [];
 
-% SOC vector
-SOC_column = cell2mat(data_accum.SOC);
+for i = 1:length(Crate_unique)
+    for j = 1:length(Diameter_unique)
+
+        % i, j - th data to write in .txt
+        SOC_vec_ij = data_accum.SOC{(i-1)*length(Diameter_unique)+j};
+        Charging_Time_vec_ij = data_accum.Charging_Time{(i-1)*length(Diameter_unique)+j};
+        
+        % length of the i, j - vector
+        L_ij = size(data_accum.SOC{(i-1)*length(Diameter_unique)+j});
+             
+ 
+        Charging_Time = [Charging_Time;...
+                    Crate_unique(i)*ones(L_ij), Diameter_unique(j)*ones(L_ij), SOC_vec_ij, Charging_Time_vec_ij];
+        
+        Selected_data = Charging_Time(Charging_Time(:,3) > 1, :);
+        Final_data = Selected_data(Selected_data(:,3) < 1.0010, :);
+
+    end
+end
 
